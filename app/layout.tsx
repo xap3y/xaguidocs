5de8892ebc9ca49c5cfe822327998d05cd6a23e0
XAP3Y <github@xap3y.eu>
2024-08-10 15:46:51 +0200

    Fix recursion

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,6 @@ const inter = Inter({
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-    <head>
-        <meta httpEquiv="refresh" content="0; URL=https://xagui.xap3y.eu/docs"/>
-    </head>
     <body>
     <RootProvider>{children}</RootProvider>
     </body>
--- app/page.tsx
+++ app/page.tsx
@@ -2,37 +2,38 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <main
-      style={{
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        textAlign: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <h1
-        style={{
-          fontSize: '2rem',
-          fontWeight: 'bold',
-          marginBottom: '1rem',
-        }}
-      >
-        XaGUI Documentation
-      </h1>
-      <p>
-        Please visit{' '}
-        <Link
-          href="/docs"
+      <main
           style={{
-            fontWeight: '600',
-            textDecoration: 'underline',
+              height: '100vh',
+              display: 'flex',
+              flexDirection: 'column',
+              textAlign: 'center',
+              justifyContent: 'center',
           }}
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
-    </main>
+      >
+          <meta httpEquiv="refresh" content="0; URL=https://xagui.xap3y.eu/docs"/>
+          <h1
+              style={{
+                  fontSize: '2rem',
+                  fontWeight: 'bold',
+                  marginBottom: '1rem',
+              }}
+          >
+              XaGUI Documentation
+          </h1>
+          <p>
+              Please visit{' '}
+              <Link
+                  href="/docs"
+                  style={{
+                      fontWeight: '600',
+                      textDecoration: 'underline',
+                  }}
+              >
+                  /docs
+              </Link>{' '}
+              and see the documentation.
+          </p>
+      </main>
   );
 }
